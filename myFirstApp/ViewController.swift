ddc25188806a022e54210b832a6b7962315736ad
gvantsa gvagvalia <gvantsa.gvagvalia1@gmail.com>
2023-05-20 14:56:58 +0400

    Initial Commit

---
+++ myFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by gvantsa gvagvalia on 5/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
