6ea7b558e0f8f689d1558539e01c4eb2eeb1822c
gvantsa gvagvalia <gvantsa.gvagvalia1@gmail.com>
2023-05-20 16:59:00 +0400

    changed name

--- myFirstApp/ViewController.swift
+++ myFirstApp/ViewController.swift
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FirstViewController: UIViewController {
+ ///komentarebii
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
