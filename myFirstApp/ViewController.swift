6f34f3dd82879d7a2346a1db4909983e7f44a7c0
gvantsa gvagvalia <gvantsa.gvagvalia1@gmail.com>
2023-05-20 17:38:14 +0400

    damatebuli - meore komentari

--- myFirstApp/ViewController.swift
+++ myFirstApp/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class FirstViewController: UIViewController {
  ///komentarebii
+    ///
+    ///axali komentariiiii
+    ///
     override func viewDidLoad() {
         super.viewDidLoad()
     }
